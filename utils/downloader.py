307bff63a9ec54df7813e3c409772cbd63752924
EvickaStudio <kirenogfx@gmail.com>
2023-11-05 10:30:07 +0100

    Update support for downloading video files and update progress bar

--- utils/downloader.py
+++ utils/downloader.py
@@ -9,7 +9,7 @@ import queue
 
 class DownloadThread(threading.Thread):
     def __init__(
-        self, url, start_byte, end_byte, filename, progress_queue, max_retries=3
+        self, url, start_byte, end_byte, filename, progress_queue, lock, max_retries=3
     ):
         super().__init__()
         self.url = url
@@ -17,28 +17,45 @@ class DownloadThread(threading.Thread):
         self.end_byte = end_byte
         self.filename = filename
         self.progress_queue = progress_queue
+        self.lock = lock
         self.max_retries = max_retries
 
     def run(self):
         headers = {"Range": f"bytes={self.start_byte}-{self.end_byte}"}
         retries = 0
+        chunk_size = 8192  # Initial chunk size
+        with requests.Session() as session:
+            while retries < self.max_retries:
+                try:
+                    response = session.get(
+                        self.url, headers=headers, stream=True, timeout=5
+                    )
+                    start_time = time.time()
+                    bytes_downloaded = 0
+                    with open(self.filename, "wb") as file:
+                        file.seek(self.start_byte)
+                        for chunk in response.iter_content(chunk_size=chunk_size):
+                            elapsed_time = time.time() - start_time
+                            download_speed = bytes_downloaded / (
+                                elapsed_time + 1e-9
+                            )  # bytes per second
+                            optimal_chunk_size = int(
+                                download_speed * 0.1
+                            )  # Aim to download a chunk in 0.1 seconds
+                            chunk_size = max(
+                                8192, min(optimal_chunk_size, 8192 * 16)
+                            )  # Clamp between 8 KB and 128 KB
 
-        while retries < self.max_retries:
-            try:
-                response = requests.get(
-                    self.url, headers=headers, stream=True, timeout=5
-                )
-                with open(self.filename, "r+b") as file:
-                    file.seek(self.start_byte)
-                    for chunk in response.iter_content(chunk_size=8192):
-                        file.write(chunk)
-                        self.progress_queue.put(len(chunk))
-                break
-            except (requests.exceptions.RequestException, ConnectionError) as e:
-                retries += 1
-                logging.warning(
-                    f"Retry {retries}/{self.max_retries}: Failed to download chunk {self.start_byte}-{self.end_byte}. Error: {e}"
-                )
+                            with self.lock:
+                                file.write(chunk)
+                            bytes_downloaded += len(chunk)
+                            self.progress_queue.put(len(chunk))
+                    break
+                except (requests.exceptions.RequestException, ConnectionError) as e:
+                    retries += 1
+                    logging.warning(
+                        f"Retry {retries}/{self.max_retries}: Failed to download chunk {self.start_byte}-{self.end_byte}. Error: {e}"
+                    )
 
 
 def download(url: str, name: str, num_threads: int = 1) -> Optional[bool]:
@@ -66,59 +83,53 @@ def download(url: str, name: str, num_threads: int = 1) -> Optional[bool]:
 
     try:
         response = requests.head(url)
-        total_size = int(response.headers["Content-Length"])
-        total_size_mb = total_size / (1024 * 1024)
-        chunk_size = total_size // num_threads
+        total_size = int(response.headers.get("Content-Length", 0))
+        if total_size == 0:
+            logging.error("Content-Length not found.")
+            return None
 
+        chunk_size = total_size // num_threads
         with open(name, "wb") as file:
             file.write(b"\0" * total_size)
 
         progress_queue = queue.Queue()
-        threads = []
-        for i in range(num_threads):
-            start_byte = i * chunk_size
-            end_byte = (
-                start_byte + chunk_size - 1 if i < num_threads - 1 else total_size - 1
+        lock = threading.Lock()
+        threads = [
+            DownloadThread(
+                url,
+                i * chunk_size,
+                (i + 1) * chunk_size - 1 if i < num_threads - 1 else total_size - 1,
+                name,
+                progress_queue,
+                lock,
             )
-            threads.append(
-                DownloadThread(url, start_byte, end_byte, name, progress_queue)
-            )
-
+            for i in range(num_threads)
+        ]
+        total_size_mb = total_size / (1024 * 1024)
         start_time = time.time()
-        for thread in threads:
-            thread.start()
-
+        [thread.start() for thread in threads]
         downloaded_size = 0
-        # Generate progress bar
         bar_length = 50
         while (
             any(thread.is_alive() for thread in threads) or not progress_queue.empty()
         ):
             while not progress_queue.empty():
                 downloaded_size += progress_queue.get()
+            time.sleep(0.5)  # Update UI less frequently
+            elapsed_time = time.time() - start_time
+            speed_kb = downloaded_size / (elapsed_time + 1e-9) / 1024
+            speed_mb = speed_kb / 1024
+            speed, unit = (speed_mb, "MB/s") if speed_mb > 1 else (speed_kb, "KB/s")
+            downloaded_size_mb = downloaded_size / (1024 * 1024)
+            percent_complete = (downloaded_size / total_size) * 100
+            progress = int(bar_length * downloaded_size // total_size)
+            progress_bar = f"[{'▇' * progress}{'-' * (bar_length - progress)}]"
+            sys.stdout.write(
+                f"\r{percent_complete:.2f}% - {progress_bar} {downloaded_size_mb:.2f}/{total_size_mb:.2f} MB - {speed:.2f} {unit}"
+            )
+            sys.stdout.flush()
 
-                # Calculate download progress and speed
-                elapsed_time = time.time() - start_time
-                speed_kb = downloaded_size / (elapsed_time + 1e-9) / 1024  # in KB/s
-                speed_mb = speed_kb / 1024  # in MB/s
-                speed, unit = (speed_mb, "MB/s") if speed_mb > 1 else (speed_kb, "KB/s")
-
-                downloaded_size_mb = downloaded_size / (1024 * 1024)
-                percent_complete = (downloaded_size / total_size) * 100
-
-                progress = int(bar_length * downloaded_size // total_size)
-                progress_bar = f"[{'#' * progress}{'.' * (bar_length - progress)}]"
-
-                sys.stdout.write(
-                    f"\r{percent_complete:.2f}% - {progress_bar} {downloaded_size_mb:.2f}/{total_size_mb:.2f} MB - {speed:.2f} {unit}"
-                )
-                sys.stdout.flush()
-
-            time.sleep(0.1)
-
-        for thread in threads:
-            thread.join()
-
+        [thread.join() for thread in threads]
         print()
         logging.info(f"Downloaded {name}")
         return True
